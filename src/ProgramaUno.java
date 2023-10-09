88ae0eb60e74b402e271af8e1eff761a1aacfa94
dsarotti <sr.sarotti@gmail.com>
2023-10-09 20:25:10 +0200

    preparado para entrega

--- src/ProgramaUno.java
+++ src/ProgramaUno.java
@@ -1,11 +1,17 @@
 /**
  * Programa que recibe un array de strings y 
  * devuelve un resultado en función de las entradas
+ * @author Dante Sarotti
+ * @version 0.1
  */
 public class ProgramaUno {
 
     /**
-     * 
+     * admite argumentos desde main() y devuelve con System.exit() los siguientes valores:
+     * Si el número de argumentos es < 1 devuelve 1
+     * Siel argumento es una cadena devuelve 2
+     * Si el argumento es un número entero menor que 0 devuelve 3
+     * En cualquier otro caso devuelve 0
      * @param args argumentos de entrada
      * @throws Exception
      */
